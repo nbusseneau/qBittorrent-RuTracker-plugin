7397189671ac26c4e9d14a887402a71e0f43f634
Nicolas Busseneau <nicolas@busseneau.fr>
2021-05-01 03:07:09 +0200

    Plugin rewrite
    Some context first:
    
    When I implemented the first version of the plugin years ago, I decided
    to use Python's default HTML parser in order not to force users to pull
    down any external parser.
    
    It was definitely a good learning experience but the default parser just
    plainly sucks since there is no concept of a tree, which you either have
    to build yourself or do without. The latter is what I did since the code
    was simple enough at first.
    
    It grew more complicated over the years as I fixed bugs and made it
    evolve following RuTracker itself evolving, and became a huge pain to
    maintain.
    
    This plugin was also one of my first forays into integrating with an
    external program via API, as a young programmer. Again, good learning
    experience, but I also missed obvious stuff.
    
    One such instance was the fix in 24fe4911a5a6000f4564babd22248750be04d6cf:
    when initially implementing, I was under the impression results should
    be consolidated and printed all at once at the end, akin to a function
    return value. This made the plugin quite slow because all pages of
    results had to be parsed before being printed at once.
    
    Now that I'm looking over the API again, it feels obvious that it was
    designed in a way that makes it possible for asynchronous retrievals to
    return results as they are coming in. Obviously, this is perfect for
    parallelization, so I happily decided to hack in a `ThreadPoolExecutor`
    map so that searches with multiple pages would concurrently retrieve
    pages once the first one got through.
    
    Doing that yielded some issues with the monolithic parser, which is when
    I quickly decided I didn't want to deal with that anymore. Hence the
    rewrite.
    
    This is the first pass, I'll do some more passes to refactor other old
    ugly pieces, such as the try/except blocks for open() methods. Already
    works great as-is though :)
    
    - Add concurrent fetches for searches with multiple pages.
    - Replace complicated HTML parser with simple regular expression.
    - Use timeit for timing tests.

--- rutracker.py
+++ rutracker.py
@@ -17,12 +17,14 @@ mirrors = [
 ]
 
 
-from html.parser import HTMLParser
+from concurrent.futures import ThreadPoolExecutor
+from html import unescape
 import http.cookiejar as cookielib
 import logging
 import os
 import re
 import tempfile
+from typing import Optional
 from urllib.error import URLError, HTTPError
 from urllib.parse import urlencode, quote, unquote
 from urllib.request import build_opener, HTTPCookieProcessor
@@ -35,7 +37,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.WARNING)
 
 
-def dict_encode(dict, encoding='cp1251'):
+def dict_encode(dict, encoding: str='cp1251') -> dict:
     """Encode dict values to encoding (default: cp1251)."""
     encoded_dict = {}
     for key in dict:
@@ -47,29 +49,42 @@ class rutracker(object):
     """RuTracker search engine plugin for qBittorrent."""
     name = 'RuTracker'
     url = 'https://rutracker.org' # We MUST produce an URL attribute at instantiation time, otherwise qBittorrent will fail to register the engine, see #15
-    
+
+    re_search_queries = re.compile(r'<a.+?href="tracker\.php?(.*?start=\d+)"')
+    re_threads= re.compile(r'<tr id="trs-tr-\d+".*?</tr>', re.S)
+    re_torrent_data = re.compile(
+        r'data-topic_id="(?P<id>\d+?)".*?>(?P<title>.+?)<'
+        r'.+?'
+        r'data-ts_text="(?P<size>\d+?)"'
+        r'.+?'
+        r'data-ts_text="(?P<seeds>[-\d]+?)"' # seeds can be negative when distribution status does not allow downloads, see https://rutracker.org/forum/viewtopic.php?t=211216#torstatus
+        r'.+?'
+        r'leechmed.+?>(?P<leech>\d+?)<', re.S
+    )
+
     @property
-    def forum_url(self):
-        return self.url + '/forum'
-        
+    def forum_url(self) -> str:
+        return self.url + '/forum/'
+
     @property
-    def login_url(self):
-        return self.forum_url + '/login.php'
-        
-    @property
-    def download_url(self):
-        return self.forum_url + '/dl.php'
-        
-    @property
-    def search_url(self):
-        return self.forum_url + '/tracker.php'
+    def login_url(self) -> str:
+        return self.forum_url + 'login.php'
+
+    def search_url(self, query: str) -> str:
+        return self.forum_url + 'tracker.php?' + query
+
+    def download_url(self, torrent_id: int) -> str:
+        return self.forum_url + 'dl.php?t=' + torrent_id
+
+    def topic_url(self, torrent_id: int) -> str:
+        return self.forum_url + 'viewtopic.php?t=' + torrent_id
 
     def __init__(self):
         """Initialize RuTracker search engine, signing in using given credentials."""
         # Initialize various objects.
         self.cj = cookielib.CookieJar()
         self.opener = build_opener(HTTPCookieProcessor(self.cj))
-        self.url = self.initialize_url() # Override url with the actual URL to be used (in case official URL isn't accessible)
+        self.url = self.__initialize_url() # Override url with the actual URL to be used (in case official URL isn't accessible)
         self.credentials = credentials
         # Add submit button additional POST param.
         self.credentials['login'] = u'Вход'
@@ -89,7 +104,7 @@ class rutracker(object):
         except (URLError, HTTPError, ValueError) as e:
             logging.error(e)
 
-    def initialize_url(self):
+    def __initialize_url(self):
         """Try to find a reachable RuTracker mirror."""
         errors = []
         for mirror in mirrors:
@@ -102,8 +117,68 @@ class rutracker(object):
                 errors.append(e)
         logging.error("Unable to resolve any RuTracker mirror -- exiting plugin search")
         raise RuntimeError("\n{}".format("\n".join([str(error) for error in errors])))
-    
-    def download_torrent(self, url):
+
+    def search(self, what: str, cat: str='all') -> None:
+        """Search for what on the search engine."""
+        self.results = {}
+        what = unquote(what)
+        logging.info("Searching for {}...".format(what))
+
+        # Execute first search pass
+        url = self.search_url('nm=' + quote(what))
+        other_pages = self.__execute_search(url, is_first=True)
+        logging.info("{} pages of results found.".format(len(other_pages)+1))
+
+        # If others pages of results have been found, repeat search for each page
+        with ThreadPoolExecutor() as executor:
+            urls = [self.search_url(unescape(page)) for page in other_pages]
+            executor.map(self.__execute_search, urls)
+
+        logging.info("{} torrents found.".format(len(self.results)))
+
+    def __execute_search(self, url: str, is_first: bool=False) -> Optional[list]:
+        """Execute search query."""
+        try:
+            response = self.opener.open(url)
+            # Only continue if response status is OK.
+            if response.getcode() != 200:
+                raise HTTPError(response.geturl(), response.getcode(), "HTTP request to {} failed with status: {}".format(url, response.getcode()), response.info(), None)
+        except (URLError, HTTPError) as e:
+            logging.error(e)
+            raise e
+
+        # Decode data
+        data = response.read().decode('cp1251')
+
+        # Look for threads/torrent_data
+        for thread in self.re_threads.findall(data):
+            match = self.re_torrent_data.search(thread)
+            if match:
+                torrent_data = match.groupdict()
+                result = self.__build_result(torrent_data)
+                self.results[torrent_data['id']] = result
+                if __name__ != '__main__':
+                    prettyPrinter(result)
+
+        # If doing first search pass, look for other pages
+        if is_first:
+            matches = self.re_search_queries.findall(data)
+            other_pages = list(dict.fromkeys(matches))
+            return other_pages
+
+    def __build_result(self, torrent_data: dict) -> dict:
+        """Map torrent data to result dict as expected by prettyPrinter."""
+        result = {}
+        result['link'] = self.download_url(torrent_data['id'])
+        result['name'] = unescape(torrent_data['title'])
+        result['size'] = torrent_data['size']
+        result['seeds'] = torrent_data['seeds']
+        result['leech'] = torrent_data['leech']
+        result['engine_url'] = 'https://rutracker.org' # We MUST use the same engine URL as the instantiation URL, otherwise downloads will fail, see #15
+        result['desc_link'] = self.topic_url(torrent_data['id'])
+        return result
+
+    def download_torrent(self, url: str) -> None:
         """Download file at url and write it to a file, print the path to the file and the url."""
         # Make temp file.
         file, path = tempfile.mkstemp('.torrent')
@@ -127,185 +202,11 @@ class rutracker(object):
         # Print file path and url.
         print(path+" "+url)
 
-    class Parser(HTMLParser):
-        """Implement a simple HTML parser to parse results pages."""
-        
-        def __init__(self, engine):
-            """Initialize the parser with url and tell him if he's on the first page of results or not."""
-            HTMLParser.__init__(self, convert_charrefs=False)
-            self.engine = engine
-            self.torrent_count = 0
-            self.other_pages = []
-            self.tr_counter = 0
-            self.cat_re = re.compile(r'tracker\.php\?f=\d+')
-            self.name_re = re.compile(r'viewtopic\.php\?t=\d+')
-            self.pages_re = re.compile(r'tracker\.php\?.*?start=(\d+)')
-            self.size_re = re.compile(r'[^.0-9a-zA-Z]+')
-            self.reset_current()
-
-        def reset_current(self):
-            """Reset current_item (i.e. torrent) to default values."""
-            self.current_item = {
-                'cat': None,
-                'name': None,
-                'link': None,
-                'size': None,
-                'size_extension': None,
-                'seeds': None,
-                'leech': None,
-                'desc_link': None,
-                'engine_url': 'https://rutracker.org', # Kludge, see #15
-            }
-
-        def handle_data(self, data):
-            """Retrieve inner text information based on rules defined in do_tag()."""
-            for key in self.current_item:
-                if self.current_item[key] == True:
-                    if key == 'size':
-                        try:
-                            value = float(data)
-                            self.current_item['size'] = self.size_re.sub(r'', data)
-                            self.current_item['size_extension'] = 'waitForEntityRef' # Flag size_extension as parsable in subsequents entityref checks
-                        except ValueError:
-                            pass # Ignore float parsing errors -- this just means we'll get the data later from <a> tag
-                    elif key == 'size_extension':
-                        self.current_item['size'] += data.strip()
-                        self.current_item['size_extension'] = 'parsed'
-                    else:
-                        self.current_item[key] = data
-                    logging.debug('handle_data: ' + str((self.tr_counter, key, data, self.current_item[key])))
-            if 'size_extension' in self.current_item and self.current_item['size_extension'] == 'parsed':
-                del self.current_item['size_extension']
-                logging.debug('handle_data: ' + str((self.tr_counter, self.current_item['size'])))
-
-        def handle_entityref(self, entity):
-            """When encountering a &nbsp; right after setting size, next handle_data() will receive size extension (e.g. 'MB', 'GB')"""
-            if entity == "nbsp" and 'size_extension' in self.current_item and self.current_item['size_extension'] == 'waitForEntityRef':
-                self.current_item['size_extension'] = True
-
-        def handle_starttag(self, tag, attrs):
-            """Pass along tag and attributes to dedicated handlers. Discard any tag without handler."""
-            try:
-                getattr(self, 'do_{}'.format(tag))(attrs)
-            except:
-                pass
-                
-        def handle_endtag(self, tag):
-            """Add last item manually on html end tag."""
-            # We add last item found manually because items are added on new
-            # <tr class="tCenter"> and not on </tr> (can't do it without the attribute).
-            if tag == 'html' and self.current_item['seeds'] != None:
-                if __name__ != "__main__": # avoid printing while developing
-                    prettyPrinter(self.current_item)
-                self.torrent_count += 1
-
-        def do_tr(self, attr):
-            """<tr class="tCenter"> is the big container for one torrent, so we store current_item and reset it."""
-            params = dict(attr)
-            try:
-                if 'tCenter' in params['class']:
-                    # Of course we won't store current_item on first <tr class="tCenter"> seen, since there's no data yet
-                    if self.tr_counter != 0:
-                        # We only store current_item if torrent is still alive.
-                        if self.current_item['seeds'] != None:
-                            if __name__ != "__main__": # avoid printing while developing
-                                prettyPrinter(self.current_item)
-                            self.torrent_count += 1
-                        else:
-                            self.tr_counter -= 1 # We decrement by one to keep a good value.
-                        logging.debug('do_tr: ' + str(self.current_item))
-                        self.reset_current()
-                    self.tr_counter += 1
-            except KeyError:
-                pass
-
-        def do_a(self, attr):
-            """<a> tags can specify torrent link in "href" or category or name. Also used to retrieve further results pages."""
-            params = dict(attr)
-            try:
-                if self.cat_re.search(params['href']):
-                    self.current_item['cat'] = True
-                elif 'data-topic_id' in params and self.name_re.search(params['href']): # data-topic_id is needed to avoid conflicts.
-                    self.current_item['desc_link'] = self.engine.forum_url + '/' + params['href']
-                    self.current_item['link'] = self.engine.download_url + '?t=' + params['data-topic_id']
-                    self.current_item['name'] = True
-                # If we're on the first page of results, we search for other pages.
-                elif self.first_page:
-                    pages = self.pages_re.search(params['href'])
-                    if pages:
-                        if pages.group(1) not in self.other_pages:
-                            self.other_pages.append(pages.group(1))
-            except KeyError:
-                pass
-
-        def do_td(self, attr):
-            """<td> tags give us number of leechers in inner text and can signal torrent size in child <a> tag OR directly in inner text."""
-            params = dict(attr)
-            try:
-                if 'tor-size' in params['class']:
-                    self.current_item['size'] = True
-                elif 'leechmed' in params['class']:
-                    self.current_item['leech'] = True
-            except KeyError:
-                pass
-
-        def do_b(self, attr):
-            """<b class="seedmed"> give us number of seeders in inner text."""
-            params = dict(attr)
-            try:
-                if 'seedmed' in params['class']:
-                    self.current_item['seeds'] = True
-            except KeyError:
-                pass
-
-        def search(self, what, start=0):
-            """Search for what starting on specified page. Defaults to first page of results."""
-            logging.debug("parse_search({}, {})".format(what, start))
-            
-            # If we're on first page of results, we'll try to find other pages
-            if start == 0:
-                self.first_page = True
-            else:
-                self.first_page = False
-            
-            try:
-                response = self.engine.opener.open('{}?nm={}&start={}'.format(self.engine.search_url, quote(what), start))
-                # Only continue if response status is OK.
-                if response.getcode() != 200:
-                    raise HTTPError(response.geturl(), response.getcode(), "HTTP request to {} failed with status: {}".format(self.engine.search_url, response.getcode()), response.info(), None)
-            except (URLError, HTTPError) as e:
-                logging.error(e)
-                raise e
-            
-            # Decode data and feed it to parser
-            data = response.read().decode('cp1251')
-            self.feed(data)
-
-    def search(self, what, cat='all'):
-        """Search for what on the search engine."""
-        # Instantiate parser
-        self.parser = self.Parser(self)
-        
-        # Decode search string
-        what = unquote(what)
-        logging.info("Searching for {}...".format(what))
-        
-        # Search on first page.
-        logging.info("Parsing page 1.")
-        self.parser.search(what)
-        
-        # If multiple pages of results have been found, repeat search for each page.
-        logging.info("{} pages of results found.".format(len(self.parser.other_pages)+1))
-        for start in self.parser.other_pages:
-            logging.info("Parsing page {}.".format(int(start)//50+1))
-            self.parser.search(what, start)
-        
-        self.parser.close()
-        logging.info("{} torrents found.".format(self.parser.torrent_count))
-
-
 # For testing purposes.
 if __name__ == "__main__":
+    import timeit
     engine = rutracker()
-    engine.search('lazerhawk')
-    engine.download_torrent('https://rutracker.org/forum/dl.php?t=4578927')
+    print(timeit.timeit(lambda: engine.search('lazerhawk'), number=1))
+    print(timeit.timeit(lambda: engine.search('ubuntu'), number=1))
+    print(timeit.timeit(lambda: engine.search('space'), number=1))
+    print(timeit.timeit(lambda: engine.download_torrent('https://rutracker.org/forum/dl.php?t=4578927'), number=1))
