db5af23832dfb7f897ec788ad530c622684b1bfe
Nicolas Busseneau <nicolas@busseneau.fr>
2021-12-01 02:06:48 +0100

    Bump to version 2.1.4

--- rutracker.py
+++ rutracker.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """RuTracker search engine plugin for qBittorrent."""
-#VERSION: 2.13
+#VERSION: 2.14
 #AUTHORS: nbusseneau (https://github.com/nbusseneau/qBittorrent-RuTracker-plugin)
 
 class Config(object):
